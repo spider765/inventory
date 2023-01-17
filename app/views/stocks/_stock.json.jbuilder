31beb54b08a5d5a605cebcf24dce1f486d01920c
Destiny <danejukwo11@gmail.com>
2023-01-17 10:20:20 +0100

    tweaked .

--- app/views/stocks/_stock.json.jbuilder
+++ app/views/stocks/_stock.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! stock, :id, :stock_name, :stock_id, :status, :created_at, :updated_at
+json.extract! stock, :id, :stock_name, :stock_amount, :status, :created_at, :updated_at
 json.url stock_url(stock, format: :json)
--- db/migrate/20230116111700_create_stocks.rb
+++ db/migrate/20230116111700_create_stocks.rb
@@ -2,8 +2,8 @@ class CreateStocks < ActiveRecord::Migration[7.0]
   def change
     create_table :stocks do |t|
       t.string :stock_name
-    t.primary_key :auto_id
-      #t.integer :stock_id
+
+      t.integer :stock_amount
       t.string :status
 
       t.timestamps
